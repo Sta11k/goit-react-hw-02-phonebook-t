a511884b13d10928e60ab083403f2b32edf4527d
Sta11k <alexandiel1984@gmail.com>
2021-10-18 23:15:33 +0300

    part2-2

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
         association: 'Other',
       },
     ],
+    filter: '',
   };
 
   hendlerSubmitForm = data => {
@@ -40,11 +41,36 @@ class App extends Component {
     });
   };
 
+  deleteContact = contactId => {
+    console.log(contactId);
+    this.setState(({ contactsOll }) => ({
+      contactsOll: contactsOll.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contactsOll, filter } = this.state;
+    const optimizedFilter = filter.toLowerCase();
+
+    return contactsOll.filter(contact =>
+      contact.name.toLowerCase().includes(optimizedFilter),
+    );
+  };
+
   render() {
+    // const {  contactsOll } = this.state;
+    const visibleContacts = this.getVisibleContacts();
     return (
       <div className="App">
         <Form propOnSubmit={this.hendlerSubmitForm} />
-        <TodoList />
+        <TodoList
+          contactsOll={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
@@ -52,20 +78,4 @@ class App extends Component {
 
 export default App;
 
-// {/* <Phonebook addNewContact={this.addNewContact} /> */}
-// state = {
-//   filter: '',
-//   name: '',
-//   number: '',
-//   contactsOll: [
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ],
-// };
-// addNewContact = contactsObj => {
-//   this.setState(prevState => {
-//     return { contactsOll: [...prevState.contactsOll, contactsObj] };
-//   });
-// };
+// https://github.com/flamesty/goit-react-hw-02-phonebook/blob/main/src/App.js
--- src/components/Todolist/Todolist.js
+++ src/components/Todolist/Todolist.js
@@ -1,22 +1,40 @@
 import s from './Todolist.module.css';
+import PropTypes from 'prop-types';
 // import { Form } from '../Forms/Form';
-import { App } from '../../App';
-//  import React, { Component } from 'react';
+// import { App } from '../../App';
+// import React, { Component } from 'react';
 
-function TodoList({ contactsOll }) {
-  // console.log( "asdasd",contactsOll)
-  //   this.props.renderList(contactsOll);
+function Todolist({ contactsOll, onDeleteContact }) {
   return (
     <ul className={s.contacts__list}>
-      {/* {contactsOll.map(contact => {
-                    return (
-                        <li key={contact.id}> <p>Association{contact.association} <span>name:{contact.name}</span> <span>{ contact.number}</span></p>
-                         </li>
-                )
-            })
-            } */}
+      {contactsOll.map(({ id, name, number, association }) => {
+        return (
+          <li key={id}>
+            {' '}
+            <p>
+              Association: {association} | <span> name: {name} | </span>
+              <span>number: {number} |</span>
+            </p>
+            <button
+              className={s.btn}
+              type="button"
+              onClick={() => onDeleteContact(id)}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }
-export default TodoList;
-// {/* <ul className="contacts__list"></ul> */}
+export default Todolist;
+
+Todolist.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
